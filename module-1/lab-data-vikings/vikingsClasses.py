a3e227d6420b072ea60d0959a60a0f5e332207a7
Carolina Castioni <carolinapcastioni@gmail.com>
2020-03-27 21:02:05 +0100

    almost done

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -55,6 +55,43 @@ class Saxon(Soldier):
 
 # War
 
+import random
 
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+    
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+    
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+    
+    def vikingAttack (self):
+        rand_viking = random.choice(self.vikingArmy)
+        rand_saxon = random.choice(self.saxonArmy)
+        if rand_viking.strength >= rand_saxon.receiveDamage():
+            saxonArmy -= rand_saxon
+            return f"result of calling {receiveDamage()} of a Saxon with the strength of a Viking"
+        else:
+            return f"result of calling {receiveDamage()} of a Saxon with the strength of a Viking"
+    
+    def saxonAttack (self):
+        rand_viking = random.choice(self.vikingArmy)
+        rand_saxon = random.choice(self.saxonArmy)
+        if rand_saxon.strength >= rand_viking.receiveDamage():
+            vikingArmy -= rand_viking
+            return f"result of calling {receiveDamage()} of a Saxon with the strength of a Viking"
+        else:
+            return f"result of calling {receiveDamage()} of a Viking with the strength of a Saxon"
+
+    def showStatus (self):
+        return f"Currently situation is:" len(vikingArmy) "and" len(saxonArmy)
+        if len(saxonArmy)<1:
+            return f"Vikings have won the war of the century!"
+        elif len(vikingArmy)<1:
+            return f"Saxons have fought for their lives and survive another day..." 
+        elif len(saxonArmy)>1 or len(vikingArmy) >1:
+            return f"Vikings and Saxons are still in the thick of battle."
+    
