3196808c6140eb1d172afc087061091108560488
Flori-87 <fcmelrod@gmail.com>
2020-03-27 16:44:31 +0100

    file with soldier and viking

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -17,9 +17,20 @@ class Soldier:
 
 
 class Viking(Soldier):
-    def __init__(self, name):
+    def __init__(self, name, health, strength):
         self.name = name
         super().__init__(health, strength)
+
+    def receiveDamage(self, damage):
+        super().receiveDamage(damage)
+        if self.health > 0:
+            return f'{self.name} has received {damage} points of damage'
+        else:
+            return f'{self.name} has died in act of combat'
+
+    def battleCry(self):
+        return f'Odin Owns You All!'
+    
 
 # Saxon
 
