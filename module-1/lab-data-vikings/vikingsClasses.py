330602fb3d1f4c0024415b5ae06a5f0051732cc0
trutorj <trutorj@gmail.com>
2020-03-27 16:19:09 +0100

    Primer commit

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,9 +1,19 @@
-MOvidas
 # Soldier
 
 
 class Soldier:
-    pass
+    def__init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    # Aquí van los métodos
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.damage = self.health - self.health
+       
+    
 
 # Viking
 
