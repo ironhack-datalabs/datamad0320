541e70579bdc998a256bf7a7f965fa2cded47ccf
AnaSenso <anasenso@gmail.com>
2020-03-27 15:57:18 +0100

    saving

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,15 +1,36 @@
 
 # Soldier
-?????
+
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack():
+        return self.strength
+    
+    def receiveDamage(slef, damage):
+        self.health = health - damage
+
 
 # Viking
 
 
 class Viking:
-    pass
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+    
+    def attack():
+        return self.strength
+    
+    def receiveDamage(slef, damage):
+        self.health = health - damage
+        if health <= 0:
+            return ("NAME has received DAMAGE points of damage")
+
 
 # Saxon
 
