59f295841271164cc1e7a120ce9bbd12f20747dc
DavEst91 <davidestebanmendoza@gmail.com>
2020-03-27 17:15:25 +0100

    Soldiers and vikings declared

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -14,10 +14,19 @@ class Soldier:
 
 # Viking
 
-
-class Viking:
-    pass
-
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name=name
+    def receiveDamage(self,damage):
+        self.health-=damage
+        if self.health>0:
+            return f'{self.name} has received {damage} points of damage'
+        else:
+            return f'{self.name} has died in act of combat'
+    def battleCry(self):
+         return 'Odin Owns You All!'
+         
 # Saxon
 
 
