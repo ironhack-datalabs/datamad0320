172be44522ebed16a5891b6499b4a87baa17d341
MiguelFdez <miguelfdezdiaz@gmail.com>
2020-03-27 20:56:43 +0100

    hecho soldier y vikings

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,7 +3,7 @@
 
 
 class Soldier:
-    def _init_(self, health, strength):
+    def __init__(self, health, strength):
         self.health = health
         self.strength = strength
     
@@ -17,8 +17,27 @@ class Soldier:
 
 # Viking
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return f'{self.name} has received {damage} points of damage'
+        else:
+            return f'{self.name} has died in act of combat'
+    
+    def battleCry(self):
+        return "Odin Owns You All!"
+        
+            
+        
 
 # Saxon
 
