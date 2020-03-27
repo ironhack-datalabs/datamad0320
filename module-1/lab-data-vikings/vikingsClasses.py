af612d16014b13292d8fa27d094365969b2b24dc
Beatriz Sanau <beatriz.sanau@gmail.com>
2020-03-27 15:46:42 +0100

    soldier

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,9 +1,21 @@
-# PR
+
 # Soldier
 
 
 class Soldier:
-    pass
+    # constructor function with two arguments: "soldier_health" and "soldier_strength"
+    # argument "self" by defect
+    def __init__(self, soldier_health, soldier_strength):
+        self.health=soldier_health       # 1st Soldier property: health
+        self.strength=soldier_strength   # 2nd Soldier property: strength
+
+    # Method 1 with 0 arguments
+    def attack(self):
+        return self.strength
+    
+    # Method 2 with one argument: "damage"
+    def receiveDamage(self, damage):
+        self.health+= (-damage)
 
 # Viking
 
