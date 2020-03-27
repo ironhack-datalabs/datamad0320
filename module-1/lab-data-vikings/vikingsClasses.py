9046e23b231330f3c57987bb9a76449667511ed2
diego-florez <diegoflorezestrada@gmail.com>
2020-03-27 19:59:55 +0100

    war

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,22 +3,73 @@
 
 
 class Soldier:
-    pass 
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        while self.health > 0:
+            return self.name + f" has received {damage} points of damage"
+        return self.name + f" has died in act of combat"
+    
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        while self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        return "A Saxon has died in combat"
+
+    
 
 # War
 
 
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+    
+    def addViking(self, Viking):
+        self.vikingArmy += [Viking()]
+    
+    def addSaxon(self, Saxon):
+        self.saxonArmy += [Saxon()]
+
+    def vikingAttack(self):
+        
+
+    
+    
+    
