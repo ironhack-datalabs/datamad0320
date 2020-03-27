a6476d81e0a3163a0d18047bd78206038f504cd1
Sergio <sergioperezalamillo@gmail.com>
2020-03-27 17:01:22 +0100

    Vamos por ellos

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,10 +1,20 @@
-
+#
 # Soldier
 
-
+    
 class Soldier:
-    pass
-
+    def __init__(self, health, strength):
+        self.health= health
+        self.strength = strength
+    def Atack(self):
+        return self.strength
+    def ReceiveDamage(self,damage):
+         
+        self.damage = damage
+        newhealth = health - damage
+        pass
+soldier = Soldier(30,50)
+print(soldier)
 # Viking
 
 
