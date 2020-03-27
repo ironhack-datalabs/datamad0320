9a7c952443e3f4e60c20957830f2316f9a8886a2
trutorj <trutorj@gmail.com>
2020-03-27 17:31:08 +0100

    Clase Viking terminado

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -21,12 +21,25 @@ class Soldier:
 class Viking(Soldier):
     def __init__(self, name, health, strength):
         super().__init__(health, strength)
+        self.name = name
 
-        # Aquí van los métodos
+    # Aquí van los métodos
+    # Reimplementamos receiveDamage
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        if self.health <= 0:
+            return f"{self.name} has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+
+
 
 
         
-    pass
+  
 
 # Saxon
 
