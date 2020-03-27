4db07c27cced64b963b8791d58884d2b5e8375b5
trutorj <trutorj@gmail.com>
2020-03-27 20:38:13 +0100

    Penúltimo commit

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -64,6 +64,45 @@ class Saxon(Soldier):
 
 # War
 
+import random
+class War:  
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+    
+    # Aquí van los métodos
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
 
-class War:
-    pass
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        # Sacar de forma aleatoria un sajón y un vikingo
+        sajon = random.choice(self.saxonArmy)
+        vikingo = random.choice(self.vikingArmy)
+
+        sajon.receiveDamage(vikingo.attack())
+        if sajon.health <= 0:
+            self.saxonArmy.remove(sajon)
+        return sajon.receiveDamage(vikingo.attack())
+
+    def saxonAttack(self):
+        sajon = random.choice(self.saxonArmy)
+        vikingo = random.choice(self.vikingArmy)
+        vikingo.receiveDamage(sajon.attack())
+        if vikingo.health <= 0:
+            self.vikingArmy.remove(vikingo)
+        return vikingo.receiveDamage(sajon.attack())
+    
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(self.saxonArmy) == 1 and len(self.vikingArmy) == 1:
+            return "Vikings and Saxons are still in the thick of battle."
+
+
+
+
