cdd69aca70ca6525d5e222f3bfb1fab787397bf8
AnaSenso <anasenso@gmail.com>
2020-03-28 11:33:08 +0100

    saving

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,4 +1,5 @@
 
+import random
 # Soldier
 
 class Soldier:
@@ -15,14 +16,13 @@ class Soldier:
 
 # Viking
 
-class Viking:
+class Viking(Soldier):
     def __init__(self, name, health, strength):
+        super().__init__(health, strength)
         self.name = name
-        self.health = health
-        self.strength = strength
     
-    def attack(self):
-        return self.strength
+    # def attack(self):
+    #     return self.strength
     
     def receiveDamage(self, damage):
         self.health -= damage
@@ -41,8 +41,8 @@ class Saxon(Soldier):
     def __init__(self, health, strength):
         super().__init__(health, strength)
     
-    def attack(self):
-        return self.strength
+    # def attack(self):
+    #     return self.strength
 
     def receiveDamage(self, damage):
         self.health -= damage
@@ -65,4 +65,28 @@ class War:
     def addSaxon(self, Saxon):
         self.saxonArmy.append(Saxon)
 
-    def vikingAttack():
+    def vikingAttack(self):
+        ransaxon = random.choice(self.saxonArmy)
+        ranviking = random.choice(self.vikingArmy)
+        if ransaxon.receiveDamage(ranviking.attack()) != 0:
+            ransaxon.health = receiveDamage(ranviking.attack())
+        else:
+            saxonArmy.remove(ransaxon)
+    
+    def saxonAttack(self):
+        ransaxon = random.choice(self.saxonArmy)
+        ranviking = random.choice(self.vikingArmy)
+        if ranviking.receiveDamage(ransaxon.attack()) != 0:
+            ranviking.health = receiveDamage(ransaxon.attack())
+        else:
+            vikingArmy.remove(ranviking)
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return (f"Vikings have won the war of the century!")
+        elif len(self.vikingArmy) == 0:
+            return (f"Saxons have fought for their lives and survive another day...")
+        else:
+            return (f"Vikings and Saxons are still in the thick of battle.")
+
+
