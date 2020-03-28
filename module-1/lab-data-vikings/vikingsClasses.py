fdce18e8e042e59d12344969d5919d3f58c0d28c
Luis Grisolía <luis.sanchez.grisolia@gmail.com>
2020-03-28 18:02:42 +0100

    soldier plus viking

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,15 +1,37 @@
 
-# Soldierr
+# Soldier
 
 
 class Soldier:
-    pass
+    def __init__ (self,health, strength):
+        self.health= health
+        self.strength= strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+
 
 # Viking
 
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        Soldier.__init__(self, health, strength)
+        self.name=name
 
-class Viking:
-    pass
+    def receiveDamage(self, damage):
+        Soldier.receiveDamage(self, damage)
+        if self.health>0:
+            return(f"{self.name} has received {damage} points of damage")
+        else:
+            return(f"{self.name} has died in act of combat")
+            
+    def battleCry(self):
+        return "Odin Owns You All!"
+
+
 
 # Saxon
 
