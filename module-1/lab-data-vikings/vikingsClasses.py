fbaf31b1f809d1628e7d52f676ac74b212e363fa
“Macarena” <macarenaumestanza@gmail.com>
2020-03-28 21:44:07 +0100

    Test 4

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -43,8 +43,41 @@ class Saxon(Soldier):
 
 # War
 
+import random
 
 class War:
-    pass
-
+    def __init__(self):
+        self.vikingArmy=[]
+        self.saxonArmy=[]
+        
+    def addViking(self,Viking):
+         self.vikingArmy.append(Viking)
+            
+    def addSaxon(self,Saxon):
+         self.saxonArmy.append(Saxon)
+            
+    def vikingAttack(self):
+        s_saxon=random.choice(self.saxonArmy)
+        s_viking=random.choice(self.vikingArmy)
+        attack=s_saxon.receiveDamage(s_viking.attack())
+        if s_saxon.health<=0:
+            self.saxonArmy.remove(s_saxon)
+        return attack
+        
+    def saxonAttack(self):
+        s_saxon=random.choice(self.saxonArmy)
+        s_viking=random.choice(self.vikingArmy)
+        attack2=s_viking.receiveDamage(s_saxon.attack())
+        if s_viking.health<=0:
+            self.vikingArmy.remove(s_viking)
+        return attack2
+    
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return f"Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return f"Saxons have fought for their lives and survive another day..."
+        else:
+            return f"Vikings and Saxons are still in the thick of battle."
+    
 
