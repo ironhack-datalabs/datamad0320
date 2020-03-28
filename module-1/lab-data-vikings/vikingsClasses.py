b9381456ed748a73d41975a42ee1eabbafac6891
AnaSenso <anasenso@gmail.com>
2020-03-28 18:03:51 +0100

    saving

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -53,7 +53,6 @@ class Saxon(Soldier):
 
 
 # War
-
 class War:
     def __init__(self):
         self.vikingArmy = []
@@ -68,16 +67,16 @@ class War:
     def vikingAttack(self):
         ransaxon = random.choice(self.saxonArmy)
         ranviking = random.choice(self.vikingArmy)
-        if ransaxon.receiveDamage(ranviking.attack()) == "A Saxon has died in combat":
-            ransaxon.health = ransaxon.receiveDamage(ranviking.attack())
+        if ransaxon.receiveDamage(ranviking.attack()) != f"A Saxon has died in combat":
+            return ransaxon.receiveDamage(ranviking.attack())
         else:
             self.saxonArmy.remove(ransaxon)
     
     def saxonAttack(self):
         rsaxon = random.choice(self.saxonArmy)
         rviking = random.choice(self.vikingArmy)
-        if rviking.receiveDamage(rsaxon.attack()) != "{rviking.name} has died in act of combat":
-            rviking.health = rviking.receiveDamage(rsaxon.attack())
+        if rviking.receiveDamage(rsaxon.attack()) != f"{rviking.name} has died in act of combat":
+            return rviking.receiveDamage(rsaxon.attack())
         else:
             self.vikingArmy.remove(rviking)
 
@@ -89,4 +88,43 @@ class War:
         else:
             return (f"Vikings and Saxons are still in the thick of battle.")
 
+
+# War
+# import random
+# class War:
+#     def __init__(self):
+#         self.vikingArmy = []
+#         self.saxonArmy = []
+    
+#     def addViking(self, viking):
+#         self.vikingArmy.append(viking)
+    
+#     def addSaxon(self, saxon):
+#         self.saxonArmy.append(saxon)
+    
+#     def vikingAttack (self):
+#         rand_viking = random.choice(self.vikingArmy)
+#         rand_saxon = random.choice(self.saxonArmy)
+#         if rand_viking.strength >= rand_saxon.receiveDamage():
+#             saxonArmy -= rand_saxon
+#             return f"result of calling {receiveDamage()} of a Saxon with the strength of a Viking"
+#         else:
+#             return f"result of calling {receiveDamage()} of a Saxon with the strength of a Viking"
+    
+#     def saxonAttack (self):
+#         rand_viking = random.choice(self.vikingArmy)
+#         rand_saxon = random.choice(self.saxonArmy)
+#         if rand_saxon.strength >= rand_viking.receiveDamage():
+#             vikingArmy -= rand_viking
+#             return f"result of calling {receiveDamage()} of a Saxon with the strength of a Viking"
+#         else:
+#             return f"result of calling {receiveDamage()} of a Viking with the strength of a Saxon"
+#     def showStatus (self):
+#         #return f"Currently situation is:" {len(vikingArmy)} "and" {len(saxonArmy)}"
+#         if len(saxonArmy)<1:
+#             return f"Vikings have won the war of the century!"
+#         elif len(vikingArmy)<1:
+#             return f"Saxons have fought for their lives and survive another day..." 
+#         elif len(saxonArmy)>1 or len(vikingArmy) >1:
+#             return f"Vikings and Saxons are still in the thick of battle."
 
