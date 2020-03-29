66835fd673d021b57231760a98e198c7cb284c95
Maria Moyano <marymp@gmail.com>
2020-03-29 12:04:13 +0200

    Guardo cambios

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -59,14 +59,29 @@ class Saxon(Soldier):
 
 class War:
     def __init__(self):
-        pass
-    def addViking():
-     pass
-    def addSaxon():
-        pass
-    def vikingAttack():
-        pass
-    def saxonAttack():
-        pass
-    def showStatus():
-        pass
+        self.vikingArmy=[]
+        self.saxonArmy=[]
+
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        return Saxon.receiveDamage(Viking.streng)
+        
+    def saxonAttack(self):
+        return Viking.receiveDamage(Saxon.streng)
+    
+    def showStatus(self):
+        result = ""
+        if len(self.vikingArmy) == 0:
+            result += "Vikings have won the war of the century!"
+        else:
+            result += "Vikings and Saxons are still in the thick of battle."
+        if len(self.saxonArmy) == 0:
+            result += "Saxons have fought for their lives and survive another day..."
+        else:
+            result += "Vikings and Saxons are still in the thick of battle."
+        return result
