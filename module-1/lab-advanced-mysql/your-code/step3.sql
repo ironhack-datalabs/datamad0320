1e726ff7aad10dbbd047c01b43d647037f241a7e
alobera <albert.jlobera@gmail.com>
2020-04-02 09:01:12 +0200

    update step3

--- module-1/lab-advanced-mysql/your-code/step3.sql
+++ module-1/lab-advanced-mysql/your-code/step3.sql
@@ -1,7 +1,8 @@
-SELECT step2.au_id, sum(tt.advance+tt.royalty)
+SELECT step2.au_id, ROUND(sum(tt.advance+tt.royalty)) AS royalty
 	FROM step2 
     INNER JOIN titleauthor AS ta
     ON ta.au_id = ta.au_id
 	INNER JOIN titles AS tt
 	ON ta.title_id = tt.title_id
 GROUP BY tt.title_id,step2.au_id
+ORDER BY royalty DESC limit 3;
