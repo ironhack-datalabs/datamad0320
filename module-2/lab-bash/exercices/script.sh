87a2283f2f3e6175a4920399b50f51de136d64a4
DavEst91 <davidestebanmendoza@gmail.com>
2020-04-07 17:15:43 +0200

    ejercicios completos

--- module-2/lab-bash/exercices/script.sh
+++ module-2/lab-bash/exercices/script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Imprime en consola `Hello World`.
 echo 'Hello World'
 
@@ -27,22 +29,77 @@ cat lorem-copy/sed.txt | tail -3
 
 #Añade `Homo homini lupus.` al final de archivo `sed.txt` dentro de la carpeta lorem-copy. 
 echo 'Homo homini lupus' >>lorem-copy/sed.txt
+
 #Visualiza las últimas 3 líneas del archivo `sed.txt` dentro de la carpeta lorem-copy. Deberías ver ahora `Homo homini lupus.`. 
 cat lorem-copy/sed.txt | tail -3
+
 #Sustituye todas las apariciones de `et` por `ET` del archivo `at.txt` dentro de la carpeta lorem-copy. Deberás usar `sed`. 
 sed -i 's/et/ET/g' lorem-copy/at.txt
+
 #Encuentra al usuario activo en el sistema.
 w
+
 #Encuentra dónde estás en tu sistema de ficheros.
 pwd
+
 #Lista los archivos que terminan por `.txt` en la carpeta lorem.
-ls lorem | grep '.txt'  
+ls lorem | grep -E '.txt\b'  
+
 #Cuenta el número de líneas que tiene el archivo `sed.txt` dentro de la carpeta lorem. 
-lorem/sed.txt
-* Cuenta el número de **archivos** que empiezan por `lorem` que están en este directorio y en directorios internos.
+wc -l lorem/sed.txt
 
-* Encuentra todas las apariciones de `et` en `at.txt` dentro de la carpeta lorem.
+#Cuenta el número de **archivos** que empiezan por `lorem` que están en este directorio y en directorios internos.
+ls -R | grep -E '\b.txt' | wc -l
 
-* Cuenta el número de apariciones del string `et` en `at.txt` dentro de la carpeta lorem. 
+#Encuentra todas las apariciones de `et` en `at.txt` dentro de la carpeta lorem.
+wc 'et' lorem/at.text
 
-*  Cuenta el número de apariciones del string `et` en todos los archivos del directorio lorem-copy. 
+#Cuenta el número de apariciones del string `et` en `at.txt` dentro de la carpeta lorem. 
+
+grep -o -i 'et' lorem/at.txt| wc -l
+
+#Cuenta el número de apariciones del string `et` en todos los archivos del directorio lorem-copy. 
+
+grep -o -i 'et' lorem/* | wc -l
+
+## Bonus
+# Almacena en una variable `name` tu nombre.
+name='David'
+
+#Imprime esa variable.
+echo $name
+
+#Crea un directorio nuevo que se llame como el contenido de la variable `name`.
+mkdir $name
+
+#Elimina ese directorio. 
+rm -r $name
+
+#Por cada archivo dentro de la carpeta `lorem` imprime el número de carácteres que tienen sus nombres. 
+#Intenta primero mostrar los archivos mediante un bucle for 
+ #   1. Imprime los ficheros
+ #   2. Imprime las longitudes de los nombres de los ficheros
+ #   3. Imprime outputs con la siguiente estructura: `lorem has 5 characters lenght`
+
+#1
+
+for file in $( ls lorem); do echo item: $file done
+
+#2
+
+for file in $( ls lorem); do echo -n "$file" | wc -m; done
+
+#3
+
+
+* Muestra los procesos de forma jerárquica que se están ejecutando en tu ordenador:
+    1. Usando el comando top o htop
+    2. Usando el comando ps con argumentos
+
+* Muestra información sobre tu procesador por pantalla
+
+* Crea 3 alias y haz que estén disponibles cada vez que inicias sesión
+
+* Comprime las carpetas lorem y lorem-copy en un archivo llamado lorem-compressed.tar.gz
+
+* Descomprime el archivo lorem-compressed.tar.gz en la carpeta lorem-uncompressed
