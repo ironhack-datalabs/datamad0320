0423e72d8a2b9ad1b5fab351fa839185fc1ee9f5
Maria Moyano <marymp@gmail.com>
2020-04-07 22:20:20 +0200

    Guardando

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -4,66 +4,76 @@ import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.rand(2, 3, 5)
 
 #4. Print a.
 
-
+print("a =",a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.full((5, 2, 3), 1)
 
 #6. Print b.
 
-
+print("b =",b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.size == b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
 
+#print(a+b)
+print("No, because a and b have different shapes")
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
-
+c = b.reshape((2, 3, 5))
+print("c =",c)
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print("a =",a)
+print("d =",d)
+#d is sum element by element a
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
-
+e = a * c
+print("e =",e)
 #13. Does e equal to a? Why or why not?
 
 
-
+print("Yes because all c-values are 1")
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
+print("d_max =",d_max)
+print("d_min =", d_min)
+print("d_mean",d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
 
-
+f = np.empty(d.shape)
+print("f =", f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
