bcc681438fdfe7a7c27e83ceae9225b26482fc11
AnaSenso <anasenso@gmail.com>
2020-04-07 20:18:11 +0200

    svaing

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -43,8 +43,8 @@ c=b.T
 d=np.concatenate((a, c))
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-print(a)
-print(d)
+#print(a)
+#print(d)
 
 #12. Multiply a and c. Assign the result to e.
 e = a*c
@@ -56,14 +56,13 @@ else:
         print("a is diffrent to e")
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
+d_max = np.amax(d)
+d_min = np.amin(d)
+d_mean = np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
-
+f=np.empty((2,3,5))
+print(d)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -74,6 +73,25 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+lis=[]
+def popu(a):
+        for b in a:
+                for c in b:
+                        for d in c:
+                                if d>np.amin(a) and d<np.mean(a):
+                                        lis.append(25)
+                                elif d>np.mean(a) and d<np.amax(a):
+                                        lis.append(75)
+                                elif d==np.mean(a):
+                                        lis.append(50)
+                                elif d ==np.amin(a):
+                                        lis.append(0)
+                                elif d==np.amax(a):
+                                        lis.append(100)
+        return lis
+
+print(popu(d))
+
 
 
 
