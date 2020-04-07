31fe0258b53222d4e53d049632102eb3e4ba7a19
AnaSenso <anasenso@gmail.com>
2020-04-07 19:24:04 +0200

    lab.numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,59 +1,59 @@
 #1. Import the NUMPY package under the name np.
-
-
+import numpy as np
+import random
 
 #2. Print the NUMPY version and the configuration.
-
-
+print(np.__version__)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
+a = np.random.randint(0,10,(2,3,5))
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
+a2 = np.random.random((2,3,5))
+a3 = np.arange(0,30).reshape((2,3,5))
 
 #4. Print a.
-
-
+#print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
-
+b= np.ones((5,3,2))
 
 #6. Print b.
-
-
+#print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
+        #print(b.T)
+def shape(a,b):
+        if a.shape == b.shape:
+                print(True)
+        else:
+                print(False)
 
-
-
+#shape(a,b)
 
 #8. Are you able to add a and b? Why or why not?
-
-
+# print(np.concatenate((a, b)))
+        # No, because all the input array dimensions for the concatenation axis must match exactly, but along dimension 2, the array at index 0 has size 5 and the array at index 1 has size 2
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+        # c= b.resize(2,3,5)
+c=b.T
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+d=np.concatenate((a, c))
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+print(a)
+print(d)
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = a*c
 
 #13. Does e equal to a? Why or why not?
-
-
-
+if a.all() == e.all():
+        print("a is equal to e")
+else:
+        print("a is diffrent to e")
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
