bbbe60227c69c38c51e259fa7a93c55de4bad59f
Miriam <miriam9495@gmail.com>
2020-04-08 16:56:49 +0200

    challenge 2 terminado

---
+++ module-1/lab-api-scavenger-game/your-code/challenge-2.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import os 
+
+from dotenv import load_dotenv
+load_dotenv()
+
+#Load the apikey
+apiKey = os.getenv("GITHUB_APIKEY")
+
+#Check if we have key
+print("WE HAVE APIKEY") if apiKey else print("NO APIKEY FOUND")
+
+def getFromGithub(path,queryParams=dict(),apiKey=""):
+    
+    # Construct the resource url
+    url = f"https://api.github.com{path}"
+    
+    # If apiKey is defined, pass a header
+    headers = {"Authorization":f"token {apiKey}"} if apiKey else {}
+
+    # Perform the request
+    res = requests.get(url, params=queryParams, headers=headers)
+    print(res.status_code, res.url)
+    
+    # Extract json from body response
+    return res.json()
+
+
+queryParams = {
+    "since":"2020-04-01T16:50:00Z",
+    "until":"2020-04-08T16:50:00Z"
+}
+
+commits = getFromGithub("/repos/ironhack-datalabs/madrid-oct-2018/commits", queryParams, apiKey=apiKey)
+
+print(len(commits))
+
+
+
