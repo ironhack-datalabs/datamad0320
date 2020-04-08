97ee6609a1f5326bb6968fbf77702b2a73d6b8eb
Miriam <miriam9495@gmail.com>
2020-04-08 19:59:58 +0200

    commit final

--- module-1/lab-api-scavenger-game/your-code/challenge-3.py
+++ module-1/lab-api-scavenger-game/your-code/challenge-3.py
@@ -28,12 +28,15 @@ def getFromGithub(path,queryParams=dict(),apiKey=""):
     # Extract json from body response
     return res.json()
 
+#Me creo una lista con el nombre de los repositorios contenidos en ironhack-datalabs/scavenger
 L1=[e['path']  for e in getFromGithub("https://api.github.com/repos/ironhack-datalabs/scavenger/contents", apiKey=apiKey)]
 L1.remove('.gitignore')
 
+#Me creo una lista con el nombre de los archivos que quiero buscar
 L2=[]
 q=['.000'+str(i)+'.scavengerhunt' if i<10 else '.00'+str(i)+'.scavengerhunt'  for i in range(25)]
 
+#Busco las URLs de las APIs de los archivos .00XX.scavengerhunt y las meto en una lista
 for e in L1:
     repos = getFromGithub(f"https://api.github.com/repos/ironhack-datalabs/scavenger/contents/{e}", apiKey=apiKey)
     for i in repos:
@@ -41,6 +44,7 @@ for e in L1:
             if b in i['url']:
                 L2.append(i['url'])
 
+#Con la URL de las APIs de los archivos .00XX.scavengerhunt, extraigo la info, es decir, nombre y contenido
 res=[]
 hol=[]
 for y in L2:
@@ -48,14 +52,16 @@ for y in L2:
     res.append(aux['content'])
     hol.append(aux['name'])
 
+#Decodifico a ASCII el contennido de cada archivo .00XX.scavengerhunt
 res_dec=[]
 for e in res:
     res_dec.append(base64.b64decode(e).decode('ascii'))
 
+#Uno el nombre con el contenido para hacer un SORT ascendente de los contenidos: .0000,.0001,.0002,.0003...
 list_tup=list(zip(hol,res_dec))
-
-list_tup=[('.0006.scavengerhunt', 'of\n'), ('.0008.scavengerhunt', 'spent\n'), ('.0012.scavengerhunt', '20\n'), ('.0007.scavengerhunt', 'time\n'), ('.0021.scavengerhunt', 'need\n'), ('.0022.scavengerhunt', 'to\n'), ('.0005.scavengerhunt', 'percent\n'), ('.0018.scavengerhunt', 'complaining\n'), ('.0016.scavengerhunt', 'is\n'), ('.0024.scavengerhunt', 'data.\n'), ('.0010.scavengerhunt', 'preparing\n'), ('.0014.scavengerhunt', 'of\n'), ('.0011.scavengerhunt', 'data,\n'), ('.0023.scavengerhunt', 'prepare\n'), ('.0020.scavengerhunt', 'the\n'), ('.0003.scavengerhunt', 'science,\n'), ('.0004.scavengerhunt', '80\n'), ('.0019.scavengerhunt', 'about\n'), ('.0017.scavengerhunt', 'spent\n'), ('.0002.scavengerhunt', 'data\n'), ('.0013.scavengerhunt', 'percent\n'), ('.0015.scavengerhunt', 'time\n'), ('.0009.scavengerhunt', 'is\n'), ('.0001.scavengerhunt', 'In\n')]
 list_tup.sort(key=operator.itemgetter(0))
+
+#Preparo el string resultado para imprimir directamente por terminal 
 flat_list=[e[1] for e in list_tup]
 result='\n'.join(flat_list)
 print(result)
